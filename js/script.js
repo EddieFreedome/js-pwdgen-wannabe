5a70c271df1ed3b7602266708c3d29ca571aac79
EddieFreedome <eddie.freed@tutanota.com>
2021-10-07 16:42:15 +0200

    Terminato esercizio JS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const nomeUtente = prompt('Inserisci il tuo nome');
+const cognomeUtente = prompt('Inserisci il tuo cognome');
+const coloreUtente = prompt('Inserisci il tuo colore preferito');
+
+alert(nomeUtente + " " + cognomeUtente + " " + coloreUtente + " " + 21 );
