ff0a0e78388a3c640cd9398b627c31984ab93719
EddieFreedome <eddie.freed@tutanota.com>
2021-10-07 16:51:43 +0200

    spostato script js prima della chiusura <body>; tolto spazi file JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const nomeUtente = prompt('Inserisci il tuo nome');
 const cognomeUtente = prompt('Inserisci il tuo cognome');
 const coloreUtente = prompt('Inserisci il tuo colore preferito');
-
 alert(nomeUtente + " " + cognomeUtente + " " + coloreUtente + " " + 21 );
